8442c4a92d04656f12ce636bd9a806c443081421
Shu Li <108698293+Cheulie@users.noreply.github.com>
2022-10-10 21:11:43 +0100

    calculated age task

--- session-3/index.js
+++ session-3/index.js
@@ -1,11 +1,11 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
-let a = 5;
-let b = 10;
-let c = a + b;
 
-console.log(c);
 
-let a = 20;
 
-console.log(a + b);
+function futureAge(name, age) {
+    const calculatedAge = age + 5
+   return "Hi " + name + " you will be " + calculatedAge + " in 5 years time"
+}
+
+console.log(futureAge("Gorgeous", 35))
