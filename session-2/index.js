6329e331003a58057dc5beb73a20eee1eedf3e1b
Shu Li <108698293+Cheulie@users.noreply.github.com>
2022-10-10 19:01:40 +0100

    an example of me using let a,b,c and console log

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,8 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+console.log("hello everyone")
+console.log('Inner selector of p', document.querySelector('.about').innerHTML)
+
+console.log('All about', document.querySelectorAll('.about'))
+
+console.log('All p:', document.querySelectorAll('p'))
--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,11 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+let a = 5;
+let b = 10;
+let c = a + b;
+
+console.log(c);
+
+let a = 20;
+
+console.log(a + b);
