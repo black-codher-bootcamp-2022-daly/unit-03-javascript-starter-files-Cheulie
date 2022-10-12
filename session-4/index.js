19eab3dc907b26f26309e54be8b66381ca3627e6
Shu Li <108698293+Cheulie@users.noreply.github.com>
2022-10-12 20:01:31 +0100

    submission of tasks 2 and 3

--- session-4/index.js
+++ session-4/index.js
@@ -1,42 +1,68 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
 
 
+const personA  = {
+    name: "Belinda",
+    age: 42,
+    location: "London",
+    likes: "travelling",
+};
 
+const personB  = {
+    name: "Anthony",
+    age: 42,
+    location: "London",
+    likes: "alcohol",    
+};
 
+const personC = {  
+    name: "Nana",
+    age: 35,
+    location: "London",
+    likes: "lobster",
+};
 
+const personD = {
+    name: "Evie",
+    age: 35,
+    location: "Hatfield",
+    likes: "dancing",
+};
 
+function biography (person) {
+    
+    return "Hi my name is " + person.name + ". I am " + person.age + " years old, I live in " + person.location + " and I like " + person.likes + "'" 
+}
 
-
-
-
-
-
-
-
-
+console.log(biography(personC));
 
 
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 6;
+const y = 4;
 
-// const addition = x + y;
+const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+console.log("Addition: x + y " + addition);
 
-// const subtraction = x - y;
+const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+console.log("Subtraction: x - y " + subtraction);
 
-// const multiplication = x * y;
+const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
+const division = x * y;
 
-// console.log("Division: x + y " + division);
+console.log("Division: x + y " + division);
+
+const squareOfX = x * x;
+
+console.log("The square of X in " + squareOfX);
+
 
 
 
@@ -54,7 +80,12 @@
 
 // ****************************************************TASK 3****************************************************
 
+const driver = "Tammy";
+const age = 42;
 
+const isOldEnoughToDrive = (age > 17);
+
+console.log( driver + " is old enough to drive");
 
 
 
