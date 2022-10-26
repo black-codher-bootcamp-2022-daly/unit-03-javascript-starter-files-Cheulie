571bb8d8f42332fd6ae8fe8764d4eaccda322971
Shu Li <108698293+Cheulie@users.noreply.github.com>
2022-10-26 21:04:40 +0100

    crazyButton task

--- session-8/js/index.js
+++ session-8/js/index.js
@@ -1,3 +1,53 @@
 import displayPopulation from "./birmingham.js";
 import { fruits } from "./fruit.js";
 import createListOfFruits from "./giveMeFruits.js";
+
+const button = document.createElement('button');
+
+const buttonText = document.createTextNode('Get Fruits')
+
+button.appendChild(buttonText)
+
+button.onClick = () => createListOfFruit(fruits)
+
+button.addEventListener
+
+document.body.appendChild(button)
+
+
+// start click me
+
+// const x = document.querySelector("#clickMe");
+// const y = document.querySelector("#hovering");
+
+// x.addEventListener("click", respondClick);
+// y.addEventListener("mouseover", respondMouseOver);
+// y.addEventListener("mouseout", respondMouseOut);
+
+// function respondMouseOver() {
+//   document.querySelector("#effect").innerHTML += "MouseOver Event" + "<br>";
+// }
+
+// function respondMouseOut() {
+//   document.querySelector("#effect").innerHTML += "MouseOut Event" + "<br>";
+// }
+
+// function respondClick() {
+//   document.querySelector("#effect").innerHTML += "Click Event" + "<br>";
+// }
+const crazyButton = document.querySelector('#remove-handler-button')
+
+function removeHandler () {
+    const box = document.querySelector('#remove-handler')
+    box.removeEventListener('mousemove', randomNumber)
+}
+
+crazyButton.addEventListener('click', removeHandler)
+
+document
+  .querySelector("#remove-handler")
+  .addEventListener("mousemove", randomNumber);
+
+function randomNumber() {
+  document.querySelector("#random-number").innerHTML = Math.random();
+}
